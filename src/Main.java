be8848535614460f8b519ca545510a2aa2c4e847
muniyaraj <muniaraj.k2004@gmail.com>
2025-10-31 09:27:05 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ import view.TicketBookingApplication;
 public class Main {
     public static void main(String[] args) {
         TicketBookingApplication.main();
+        System.out.println("Commit");
     }
 }
